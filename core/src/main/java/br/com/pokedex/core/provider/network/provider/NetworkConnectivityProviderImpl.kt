7aa609a111bc7b175bc947a61fb5dcf5a2602eeb
Lucas Moro <lucas.rmoro1@gmail.com>
2025-06-09 13:39:34 -0300

    Removing unused imports in network connectivity provider

--- core/src/main/java/br/com/pokedex/core/provider/network/provider/NetworkConnectivityProviderImpl.kt
+++ core/src/main/java/br/com/pokedex/core/provider/network/provider/NetworkConnectivityProviderImpl.kt
@@ -5,11 +5,7 @@ import android.net.ConnectivityManager
 import android.net.ConnectivityManager.NetworkCallback
 import android.net.Network
 import android.net.NetworkCapabilities
-import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
 import android.net.NetworkCapabilities.NET_CAPABILITY_VALIDATED
-import android.net.NetworkCapabilities.TRANSPORT_CELLULAR
-import android.net.NetworkCapabilities.TRANSPORT_WIFI
-import android.net.NetworkRequest
 import androidx.core.content.getSystemService
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
