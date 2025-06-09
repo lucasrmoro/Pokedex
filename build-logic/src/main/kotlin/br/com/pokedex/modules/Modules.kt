6cea927beb98fa09500acb0c776c9ee7c835020a
Lucas Moro <lucas.rmoro1@gmail.com>
2025-06-09 12:30:07 -0300

    Removing unused constant

--- build-logic/src/main/kotlin/br/com/pokedex/modules/Modules.kt
+++ build-logic/src/main/kotlin/br/com/pokedex/modules/Modules.kt
@@ -1,7 +1,6 @@
 package br.com.pokedex.modules
 
 object Modules {
-    const val APP = ":app"
     const val CORE = ":core"
     const val CORE_NETWORK = ":core-network"
     const val CORE_UI = ":core-ui"
